b9dc7430dc471af82c448002d05d0961e7e27e46
Mi <74912354+milehagen@users.noreply.github.com>
2022-02-15 10:33:09 +0100

    changed background image

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import weddinginvite from './weddinginvite.svg';
-import huyAnneHoyBilde from './huyAnneHoyBilde.png';
 import text from './text.png';
 
 import './App.css';
@@ -7,20 +6,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       <p>Save The Date</p>
-      </header>
       <div class="container all-content">
   <div class="row">
     <div class="col">
-       <img src={weddinginvite} className="wedding-invite" alt="wedding invitation" />
-    <span> 
-    <img src={text} className="text-next-to-image" alt="text" /> 
-    </span>
+       <h1 className="website-heading">HUY & ANNE </h1>
         </div>
            
         <div class="col">
-          <img src={huyAnneHoyBilde} className="huy-anne-bilde" alt="Bilde Huy og Anne" />
+          <img src={weddinginvite} className="wedding-invite" alt="wedding invitation" />
           <span> 
     <img src={text} className="text-next-to-image" alt="text" /> 
     </span>
